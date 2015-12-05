2c13530cd3966a266b24d0cdd36da4a0b56d408b
Ashlin Aronin <ashilnaronin@gmail.com>
2015-12-05 15:34:20 -0800

    Get positions from dragged objects

--- drag-logic.js
+++ drag-logic.js
@@ -3,26 +3,52 @@
 var numSines = 5;
 var numSquares = 5;
 var numTriangles = 5;
+var notePositions = [];
+var numHorizontal = 8;
+var numVertical = 8;
+var sines = [], triangles = [], squares = [];
+
+// Populate 2d notes array with nulls
+for (var i = 0; i < numHorizontal; i++) {
+  notePositions[i] = [];
+
+  for (var j = 0; j < numVertical; j++) {
+    notePositions[i][j] = null;
+  }
+}
 
 for (var i = 0; i < numSines; i++) {
-  $('#selection-zone').prepend('<div class="sine note ui-widget-content">sine</div>');
+  var newSine = $('#sequencer-grid').append('<div class="sine note ui-widget-content">sine</div>');
+  sines.push(newSine);
 }
 
 for (var i = 0; i < numSquares; i++) {
-  $('#selection-zone').prepend('<div class="square note ui-widget-content">square</div>');
+  var newSquare = $('#sequencer-grid').append('<div class="square note ui-widget-content">square</div>');
+  squares.push(newSquare);
 }
 
 for (var i = 0; i < numTriangles; i++) {
-  $('#selection-zone').prepend('<div class="triangle note ui-widget-content">tri</div>');
+  var newTriangle = $('#sequencer-grid').append('<div class="triangle note ui-widget-content">tri</div>');
+  triangles.push(newTriangle);
 }
 
 
+// Update the note positions array when notes are dragged
+// Only applied when they are within bounds of sequencer box
 $(".note").draggable({
-  start: function() {
-    $(this).addClass('dragging');
+  start: function(event, ui) {
+    var xPos = (ui.position.left / 50);
+    var yPos = (ui.position.top / 50);
+    if ((xPos >= 0) && (yPos >= 0)) {
+      notePositions[xPos][yPos] = null;
+    }
   },
-  stop: function() {
-    $(this).removeClass('dragging');
+  stop: function(event, ui) {
+    var xPos = (ui.position.left / 50);
+    var yPos = (ui.position.top / 50);
+    if ((xPos >= 0) && (yPos >= 0)) {
+      notePositions[xPos][yPos] = ui.helper[0].classList[0];
+    }
   }
 });
 
