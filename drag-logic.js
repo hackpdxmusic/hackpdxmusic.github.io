af1c932552cbb34347cab397a19de114050487ff
Ashlin Aronin <ashilnaronin@gmail.com>
2015-12-05 14:46:52 -0800

    Add drop shadow on drag

--- drag-logic.js
+++ drag-logic.js
@@ -1,4 +1,20 @@
 // todo: make object z-index on top when its being dragged
+// populate dom with lots of sines
+var numSines = 5;
+var numSquares = 5;
+var numTriangles = 5;
+
+for (var i = 0; i < numSines; i++) {
+  $('#selection-zone').prepend('<div class="sine note ui-widget-content">sine</div>');
+}
+
+for (var i = 0; i < numSquares; i++) {
+  $('#selection-zone').prepend('<div class="square note ui-widget-content">square</div>');
+}
+
+for (var i = 0; i < numTriangles; i++) {
+  $('#selection-zone').prepend('<div class="triangle note ui-widget-content">tri</div>');
+}
 
 
 $(".note").draggable({
