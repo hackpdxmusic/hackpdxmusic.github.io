580fa785b65ebc26c8593121a03ae129b062edf5
Ashlin Aronin <ashilnaronin@gmail.com>
2015-12-05 14:30:56 -0800

    Hide multiple icon generation for now

---
+++ app.js
@@ -0,0 +1,11 @@
+// $(document).ready(function() {
+//   // populate dom with lots of sines
+//   var numSines = 5;
+//   var numSquares = 5;
+//   var numTriangles = 5;
+//
+//   for (var i = 0; i < numSines; i++) {
+//     $('#selection-zone').prepend('<div class="sine note ui-widget-content">sine</div>');
+//   }
+//
+// });
--- drag-logic.js
+++ drag-logic.js
@@ -1,7 +1,15 @@
 // todo: make object z-index on top when its being dragged
 
 
-$(".note").draggable();
+$(".note").draggable({
+  start: function() {
+    $(this).addClass('dragging');
+  },
+  stop: function() {
+    $(this).removeClass('dragging');
+  }
+});
+
 
 $("#sequencer-grid").droppable({
     over: function(event, ui) {
